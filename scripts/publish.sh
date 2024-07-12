eba6c9bee31ef0ad2f55551c8017e9d8981719bf
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2024-07-12 20:28:35 +0200

    Update publish script and add npm script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,15 +3,18 @@
 # Sets the package ready for NPM: only uploads the contents of the dist/
 # directory.
 
-if [[ ! -d dist ]]; then
+PROJECT_DIR="$(dirname "$(readlink -f "${0##*/}")")"
+
+if [[ ! -d ${PROJECT_DIR}/dist ]]; then
     printf "dist/ directory does not exist. Exiting...\n"
     exit 1
 fi
 
+
+cd "${PROJECT_DIR}" || exit
+
 cp package* README.md dist
 
-{
-    cd dist || exit
-    rm ./*.LICENSE
-    npm publish
-}
+cd dist || exit
+rm ./*LICENSE*
+npm publish
